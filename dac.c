ee335690b6bf1d86e5b0f1fd3281f7a471021833
Roman Vasilyev <roman@hemisphere.bitzermobile.dev>
2014-10-29 20:09:01 -0700

    some code for dac

--- dac.c
+++ dac.c
@@ -24,10 +24,111 @@
     Copyright (c) 2011 Sungeun K. Jeon
 */ 
 
+#include "system.h"
+
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+
+#define SLA_W 0xC0 //write address
+
+static void
+i2c_bitrate_set(unsigned short bitrateKHz)
+{
+    unsigned char bitrate_div;
+    // set i2c bitrate
+    // SCL freq = F_CPU/(16+2*TWBR))
+    //#ifdef TWPS0
+    // for processors with additional bitrate division (mega128)
+    // SCL freq = F_CPU/(16+2*TWBR*4^TWPS)
+    // set TWPS to zero
+    cbi(TWSR, TWPS0);
+    cbi(TWSR, TWPS1);
+    //#endif
+    // calculate bitrate division   
+    bitrate_div = ((F_CPU/1000l)/bitrateKHz);
+    if(bitrate_div >= 16)
+        bitrate_div = (bitrate_div-16)/2;
+    TWBR = bitrate_div;
+}
+
+void
+i2cSendStart(void)
+{
+    // send start condition
+    TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+}
+
+void i2cSendStop(void)
+{
+    // transmit stop condition
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+}
+
+void i2cWaitForComplete(void)
+{
+    // wait for i2c interface to complete operation
+    while (!(TWCR & (1<<TWINT)));
+}
+
+void i2cSendByte(unsigned char data)
+{
+    // save data to the TWDR
+    TWDR = data;
+    // begin send
+    TWCR = (1<<TWINT)|(1<<TWEN);
+}
+
+#define Vout 3000//binary value going into register, 0-4095
+
 // Initialize and setup the stepper motor subsystem
 void
 stepper_init()
 {
+    // set pull-up resistors on I2C bus pins
+    //sbi(PORTC, 0);    // i2c SCL on ATmega163,323,16,32,etc
+    //sbi(PORTC, 1);    // i2c SDA on ATmega163,323,16,32,etc
+    //sbi(PORTD, 0);    // i2c SCL on ATmega128,64
+    //sbi(PORTD, 1);    // i2c SDA on ATmega128,64
+
+    // set i2c bit rate to 40KHz
+    i2c_bitrate_set(100);
+    // enable TWI (two-wire interface)
+    sbi(TWCR, TWEN);
+
+    //initilize I2C hardware
+    TWCR = 0x00;
+    TWBR = 8;
+    cbi(TWCR, TWEA);    
+    sbi(TWCR, TWEN);
+
+    //Send start condition 
+    i2cSendStart(); 
+    i2cWaitForComplete();
+
+    // send slave device address with write
+    i2cSendByte(SLA_W); 
+    i2cWaitForComplete();   
+
+    //set control bytes
+    char lVout = Vout & 255;
+    char hVout = (Vout>>8) & 0x0F;
+
+    // send first byte to MCP
+    TWDR = hVout;
+    // begin send
+    TWCR = (1<<TWINT)|(1<<TWEN);    
+    i2cWaitForComplete();
+
+    // send second byte to MCP
+    TWDR = lVout;
+    // begin send
+    TWCR = (1<<TWINT)|(1<<TWEN);    
+    i2cWaitForComplete();
+
+    //send stop condition
+    i2cSendStop();
+
+    TWCR = 0x00;//stop I2C
 }
 
 // Enable steppers, but cycle does not start unless called by motion control or runtime command.
